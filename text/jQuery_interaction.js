7f76099d5dbeb228cc80f1a4bf7cd203e338c87f
tdeniaud <58684749+tdeniaud@users.noreply.github.com>
2019-12-17 19:56:40 +0100

    stop work for today, MAJ bloc BSI desc

--- text/jQuery_interaction.js
+++ text/jQuery_interaction.js
@@ -3,3 +3,17 @@ $(document).ready(function(){
     $(document.getElementById("reseaux-photo")).hover(function(){$(document.getElementById("desc")).css("display","block");}, function(){$(document.getElementById("desc")).css("display","none");});
 
   });
+
+  $(document).ready(function(){
+
+    $(document.getElementById("reseaux-photo2")).hover(function(){$(document.getElementById("desc2")).css("display","block");}, 
+    function(){$(document.getElementById("desc2")).css("display","none");});
+
+  });
+
+  $(document).ready(function(){
+
+    $(document.getElementById("reseaux-photo3")).hover(function(){$(document.getElementById("desc3")).css("display","block");},
+     function(){$(document.getElementById("desc3")).css("display","none");});
+
+  });
