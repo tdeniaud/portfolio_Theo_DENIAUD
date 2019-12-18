0c6774b89c7afd5ccb915febe9a9b8cb14bb19a2
tdeniaud <58684749+tdeniaud@users.noreply.github.com>
2019-12-18 14:33:21 +0100

    modification JS et création variable CSS hauteur bloc alpha

--- text/jQuery_interaction.js
+++ text/jQuery_interaction.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
 
-    $(document.getElementById("reseaux-photo")).hover(function(){$(document.getElementById("desc")).css("display","block");}, function(){$(document.getElementById("desc")).css("display","none");});
-
+    $(document.getElementById("reseaux-photo")).hover(function(){$(document.getElementById("desc")).css("display","block");}, 
+    function(){$(document.getElementById("desc")).css("display","none");});
+    $(document.getElementById("desc")).hover(function(){$(document.getElementById("desc")).css("display","block");}, 
+    function(){$(document.getElementById("desc")).css("display","none");});
   });
 
   $(document).ready(function(){
 
     $(document.getElementById("reseaux-photo2")).hover(function(){$(document.getElementById("desc2")).css("display","block");}, 
+    function(){$(document.getElementById("desc2")).css("display","none");});
+    $(document.getElementById("desc2")).hover(function(){$(document.getElementById("desc2")).css("display","block");}, 
     function(){$(document.getElementById("desc2")).css("display","none");});
 
   });
@@ -15,5 +19,7 @@ $(document).ready(function(){
 
     $(document.getElementById("reseaux-photo3")).hover(function(){$(document.getElementById("desc3")).css("display","block");},
      function(){$(document.getElementById("desc3")).css("display","none");});
+     $(document.getElementById("desc3")).hover(function(){$(document.getElementById("desc3")).css("display","block");}, 
+    function(){$(document.getElementById("desc3")).css("display","none");});
 
   });
