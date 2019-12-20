c4b70e1ed73eae1175ed262386a77cc0af789a17
tdeniaud <58684749+tdeniaud@users.noreply.github.com>
2019-12-20 15:30:28 +0100

    fin CCN rendu du mois de décembre

--- text/typewriter.js
+++ text/typewriter.js
@@ -1,11 +1,11 @@
-var j = 0;
-var titre = 'CV de Théo DENIAUD'; 
+var j = 0; 
+var titre = 'CV de Théo DENIAUD'; /* Texte à écrire */
 
 
 var speed = 100; /* Définition de la vitesse d'écriture du texte en ms */
 
 
-function typeWriter_titre() {
+function typeWriter_titre() { /* Afficher la variable titre, lettre par lettre */
 
 if (j < titre.length) {
 
