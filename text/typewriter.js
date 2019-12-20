247eef3d20983775a92c0f009056afd6823f4c56
tdeniaud <58684749+tdeniaud@users.noreply.github.com>
2019-12-20 09:25:52 +0100

    ajout JS sur titre

--- text/typewriter.js
+++ text/typewriter.js
@@ -1,11 +1,33 @@
 var i = 0;
-var txt = 'Mes compétences'; 
-var speed = 50; 
+var j = 0;
+var k = 0;
+var skill = 'Mes compétences'; 
+var titre = 'CV de Théo DENIAUD'; 
+var xp = 'Mon expérience'; 
+
+
+var speed = 100; 
 
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("skill").innerHTML += txt.charAt(i);
+  if (i < skill.length) {
+    document.getElementById("skill").innerHTML += skill.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
 }
+
+function typeWriter_titre() {
+if (j < titre.length) {
+  document.getElementById("titre").innerHTML += titre.charAt(j);
+  j++;
+  setTimeout(typeWriter_titre, speed);
+}
+}
+
+function typeWriter_xp() {
+  if (k < xp.length) {
+    document.getElementById("xp").innerHTML += xp.charAt(k);
+    k++;
+    setTimeout(typeWriter_xp, speed);
+  }
+  }
